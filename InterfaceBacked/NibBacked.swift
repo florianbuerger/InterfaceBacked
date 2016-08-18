44bebbf4a60e2ef22cb2fb2fd4a865a795e8d06e
Florian Buerger <florian@florianbuerger.com>
2016-08-18 11:12:40 +0700

    Run Xcode converter --> Swift3

--- InterfaceBacked/NibBacked.swift
+++ InterfaceBacked/NibBacked.swift
@@ -1,30 +1,30 @@
 import UIKit
 
 public protocol NibBacked: class {
-    static func newFromNib(withName name:String, bundle:NSBundle, owner: AnyObject?, options: [NSObject : AnyObject]?) -> Self
+    static func newFromNib(withName name:String, bundle:Bundle, owner: AnyObject?, options: [NSObject : AnyObject]?) -> Self
 }
 
 public extension NibBacked {
-    static func newFromNib(withName name:String = String(Self.self), bundle:NSBundle = NSBundle(forClass: Self.self), owner: AnyObject? = nil, options: [NSObject : AnyObject]? = nil) -> Self {
+    static func newFromNib(withName name:String = String(Self.self), bundle:Bundle = Bundle(for: Self.self), owner: AnyObject? = nil, options: [NSObject : AnyObject]? = nil) -> Self {
         let nib = UINib(nibName: name, bundle: bundle)
-        let view = nib.instantiateWithOwner(owner, options: options).first as! Self
+        let view = nib.instantiate(withOwner: owner, options: options).first as! Self
         return view
     }
 }
 
 public protocol NibBackedCell: class {
-    static func cellNib(inBundle bundle: NSBundle) -> UINib
+    static func cellNib(inBundle bundle: Bundle) -> UINib
     static func identifier() -> String
 }
 
 public extension NibBackedCell {
-    static func cellNib(inBundle bundle: NSBundle = NSBundle(forClass: Self.self)) -> UINib {
-        let realName = NSStringFromClass(self as AnyClass).componentsSeparatedByString(".").last!
+    static func cellNib(inBundle bundle: Bundle = Bundle(for: Self.self)) -> UINib {
+        let realName = NSStringFromClass(self as AnyClass).components(separatedBy: ".").last!
         let nib = UINib(nibName: realName, bundle: bundle)
         return nib
     }
     static func identifier() -> String {
-        let realName = NSStringFromClass(self as AnyClass).componentsSeparatedByString(".").last!
+        let realName = NSStringFromClass(self as AnyClass).components(separatedBy: ".").last!
         return realName
     }
 }
--- InterfaceBacked/StoryboardBacked.swift
+++ InterfaceBacked/StoryboardBacked.swift
@@ -1,11 +1,11 @@
 import UIKit
 
 public protocol StoryboardBacked:class {
-    static func newFromStoryboard(withName name:String, bundle:NSBundle) -> Self
+    static func newFromStoryboard(withName name:String, bundle:Bundle) -> Self
 }
 
 public extension StoryboardBacked {
-    static func newFromStoryboard(withName name: String = String(Self.self), bundle: NSBundle = NSBundle(forClass: Self.self)) -> Self {
+    static func newFromStoryboard(withName name: String = String(Self.self), bundle: Bundle = Bundle(for: Self.self)) -> Self {
         let storyboard = UIStoryboard(name: name, bundle: bundle)
         let controller = storyboard.instantiateInitialViewController()! as! Self
         return controller
